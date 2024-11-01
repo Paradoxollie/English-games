1e3a43b5116d3715d9bfdac4e63aef8775a6aea7
Paradoxollie <want.to@hotmail.fr>
2024-11-01 08:42:11 +0100

    Message de commit

--- data/convert.py
+++ data/convert.py
@@ -5,7 +5,7 @@ with open('words.json', 'r') as file:
     words = json.load(file)
 
 # Filtrer les mots entre 3 et 5 lettres
-filtered_words = [word for word in words if 8 <= len(word) <= 22]
+filtered_words = [word for word in words if 5 <= len(word) <= 5]
 
 # Afficher les mots dans le format souhaité
 formatted_output = ", ".join(f'"{word}"' for word in filtered_words)
